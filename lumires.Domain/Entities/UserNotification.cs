6ab49ddf87e9a6a0e17bb82d9d7b7891bf42eb90
Andrii Odintsov <andrii.odintsov@dvigunity.com>
2026-02-20 14:12:18 +0200

    added required for the user notification entity

--- lumires.Domain/Entities/UserNotification.cs
+++ lumires.Domain/Entities/UserNotification.cs
@@ -5,10 +5,10 @@ namespace Domain.Entities;
 public sealed class UserNotification
 {
     public Guid Id { get; init; } = Guid.CreateVersion7();
-    public Guid UserId { get; init; }
-    public NotificationType Type { get; init; }
-    public string SenderId { get; init; } = null!;
+    public required Guid UserId { get; init; }
+    public required NotificationType Type { get; init; }
+    public required string SenderId { get; init; }
     public string? TargetId { get; init; }
-    public DateTime CreatedAt { get; init; }
+    public required DateTime CreatedAt { get; init; }
     public DateTime? ReadAt { get; init; }
 }
